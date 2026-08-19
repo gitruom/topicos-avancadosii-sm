fcd4f011075d57228f0f0e1dccc3a57daa10e47f
GUILHERME MOURA FREITAS <guilhermefreitas006@cs.cruzeirodosul.edu.br>
2026-08-19 20:10:44 -0300

    Commit inicial CI/CD

---
+++ src/validator.ts
@@ -0,0 +1,9 @@
+function validarChamado(titulo: string): boolean {
+    if (!titulo) {
+        throw new Error("O título do chamado não pode ser vazio.");
+    }
+
+    return true;
+}
+
+export default validarChamado;
